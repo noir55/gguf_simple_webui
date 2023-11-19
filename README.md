98eee3a4e941cbcd2cd1ecfc5a0e32a99c4a1b35
noir55 <bl_phil@nifty.com>
2023-11-19 19:06:17 +0900

    ファイル作成

--- README.md
+++ README.md
@@ -1,2 +1,213 @@
-# gguf_simple_webui
-llama.cppで実行するGGUF形式のLLM用の簡易WebUIです。
+# GGUF Simple WebUI
+llama-cpp-python(llama.cpp)で実行するGGUF形式のLLM用の簡易Webチャットインタフェースです。
+
+# 機能
+- ブラウザで開いたWebインタフェース上で、LLMとのチャットができます
+- ストリーミング(生成中の表示)に対応
+- GPUを搭載していない環境でも実行可能 (処理速度はGPU使用時より遅いです)
+
+# 動作要件
+- Rocky Linux 8.8 上の Python 3.9.13 の環境で本スクリプトを作成していますが、おそらく [llama-cpp-python](https://github.com/abetlen/llama-cpp-python)、[gradio](https://github.com/gradio-app/gradio) モジュールがインストールされたPython環境があれば動作すると思います
+- GPUはNVIDIA GPU、CUDA 環境で確認しています (GeForce RTX 3060、CUDA 11.7 環境で作成しています)
+- GPUを使用するには、llama-cpp-pythonがGPU対応するようインストールする必要があります。インストール方法は [llama-cpp-python](https://github.com/abetlen/llama-cpp-python) を参照してください。
+
+# 使い方
+
+## 起動方法
+
+- `gguf-webui.py` 内の15行目以降に各種設定項目がありますので、実行したいモデル、使用したいプロンプト、WebUIで使用するIPアドレスや ポート番号などを記述して起動してください
+```bash
+$ python3 gguf-webui.py
+```
+`gguf-webui.py` のファイル名に指定はないため、ファイルを任意の名前でコピーして、モデルごとや設定ごとに使い分けることができます
+
+- 実行時のオプションで `gguf-webui.py` 内の設定を上書きできるため、コマンドオプションのみで設定を指定して起動することも可能です
+
+- 実行前にGGUF形式のモデルを入手して任意のディレクトリ内に配置してください
+
+Vicuna 13B v1.5モデルでの実行コマンド例
+```bash
+$ python3 gguf-webui.py \
+        --model ~/GGUF_Model/vicuna-13b-v1.5.Q4_K_M.gguf \
+        --gpu on \
+        --gpu-layers 30 \
+        --max-new-tokens 1024 \
+        --prompt-type vicuna \
+        --prompt-threshold 3072 \
+        --prompt-deleted 3071 \
+        --title "Vicuna 13B v1.5 GGUF Chat"
+```
+
+Vicuna 13B v1.5 16kモデルでの実行コマンド例 (コンテキスト拡張されているため、Rope Scalingの設定が必要)
+```bash
+$ python3 llm-webui.py \
+        --model ~/GGUF_Model/vicuna-13b-v1.5-16k.Q4_K_M.gguf \
+        --gpu on \
+        --gpu-layers 30 \
+        --context-size 16384 \
+        --max-new-tokens 4096 \
+        --rope-freq-base 10000.0 \
+        --rope-freq-scale 0.25 \
+        --prompt-type vicuna \
+        --prompt-threshold 12288 \
+        --prompt-deleted 12287 \
+        --title "Vicuna 13B v1.5 16K GGUF Chat"
+```
+
+- 起動したら、ブラウザで http://127.0.0.1:7860 (IPアドレス、ポート番号を変更した場合はそれに合わせてください)を開いてください
+
+## 設定
+
+以下のオプションが指定可能です。
+
+| オプション                              | 説明                                                            |
+| ------------------------------------ | ----------------------------------------------------------------- |
+| --help                               | 指定可能なオプションの情報を出力する                                |
+| --model <モデルファイルのパス>             | 保存したモデルファイルのパスを指定する   |
+| --model-type <モデルタイプ名>        | モデルタイプを指定する。現時点では `llama` のみのためあえて指定する必要はない |
+| --context-type <プロンプトタイプ名>   | モデルが扱えるコンテキストサイズを指定する。 |
+| --gpu <on/off>   | GPUを使用するかどうかを指定する。 |
+| --gpu-layers <レイヤ数>   | GPUを使用する場合、GPUで処理を行うレイヤ数を指定する。この値が大きいほどGPUメモリが必要になる。 |
+| --tensor-split <使用させたいGPUメモリ量の比率>   | 複数GPUが利用可能な場合、カンマ区切りでそれぞれのGPUが使用するメモリ量の比率を `0.3,0,7` のように指定する。 |
+| --threads <スレッド数>   | CPUコアをいくつ使用して処理するかを指定する。 |
+| --use-mmap <on/off>   | CPUで処理する場合、MMAPを使用するかどうかを指定する。 |
+| --use-mlock <on/off>   | CPUで処理する場合、メインメモリ上にモデルを常駐させるかどうかを指定する。 |
+| --prompt-type <プロンプトタイプ名>   | 使用するプロンプトテンプレートを指定する。基本的にはモデルの学習に使用されたテンプレートを指定する。詳細は下の項目を参照 |
+| --prompt-threshold <トークン数>      | プロンプト生成時に会話履歴を含めたトークン数がここで設定した数を超えると会話履歴が古い順に削除される |
+| --prompt-deleted <トークン数>        | `--prompt-threshold` 設定値を超えて会話履歴が削除される場合、ここで指定したトークン数以 下になるまで削除される |
+| --max-new-tokens <トークン数>        | モデルが一度に生成する最大トークン数 |
+| --temperature <Temperature値>        | 値を大きくすると多様な出力を行うようになる。0～1の間の値を設定する |
+| --repetition-penalty <繰り返しペナルティ値>        | 値を大きくすると、繰り返しが発生しにくくなる。1.0だとペナルティなしとなる |
+| --setting-visible <on/off>        | Advanced SettingsをWebUI上に表示するかどうか |
+| --rope-freq-base <Rope Scaling Base値>      | `10000.0` を指定しておけば大丈夫そう |
+| --rope-freq-scale <Rope Scaling 値>      | llama2ベースのモデルの場合、コンテキストサイズが4kのモデルの場合は `1.0` を、コンテキストサイズが16kのモデルの場合は `0.25` を指定しておけば大丈夫そう |
+| --host <IPアドレス>                  | WebUIがバインドするアドレスを指定する。同じPC上のブラウザから使用する場合は `127.0.0.1` でよい |
+| --port <ポート番号>                  | WebUIがバインドするポート番号を指定する。他のプログラムが使用していなければいくつでもよ いが、Linux上で実行する場合、1024以下を指定するには通常root権限が必要 |
+| --title "<タイトル文字列>"           | WebUIの最上部に表示するタイトルを任意に指定可能 |
+| --debug <on/off>                     | コンソールにデバッグ情報を表示 |
+
+
+## プロンプトタイプについて
+
+主に、日本語対応のモデルで使用されるプロンプトが使用可能です。
+モデルの学習時に使用されたプロンプトを指定することで精度の高い回答が期待できます。
+
+### プロンプトタイプ名 `none`
+プロンプトは使用せず、ユーザの入力した続きの文章をモデルが出力する形式。ファインチューニング前提の素のモデルを試す時などに使う
+```
+<ユーザの入力した文章><モデルの出力した文章>
+```
+
+
+### プロンプトタイプ名 `vicuna`
+プロンプト形式
+```
+A chat between a curious user and an artificial intelligence assistant.
+The assistant gives helpful, detailed, and polite answers to the user's questions.
+
+USER: <ユーザの入力した文章>
+ASSISTANT: <モデルの出力した文章>
+```
+
+
+### プロンプトタイプ名 `alpaca`
+プロンプト形式
+```
+Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+### Instruction:
+<ユーザの入力した文章>
+
+### Response:
+<モデルの出力した文章>
+```
+
+
+### プロンプトタイプ名 `llama2`
+プロンプト形式
+```
+System: You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+User: <ユーザの入力した文章>
+Assistant: <モデルの出力した文章>
+```
+
+
+### プロンプトタイプ名 `openbuddy`
+プロンプト形式
+```
+You are a helpful, respectful and honest INTP-T AI Assistant named Buddy. You are talking to a human User.
+Always answer as helpfully and logically as possible, while being safe. Your answers should not include any harmful, political, religious, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+You like to use emojis. You can speak fluently in many languages, for example: English, Chinese.
+You cannot access theinternet, but you have vast knowledge, cutoff: 2021-09.
+You are trained by OpenBuddy team, (https://openbuddy.ai, https://github.com/OpenBuddy/OpenBuddy), you are based on LLaMA and Falcon transformers model, not related to GPT or OpenAI.
+
+User: <ユーザの入力した文章>
+Assistant: <モデルの出力した文章>
+```
+
+
+### プロンプトタイプ名 `airoboros`
+プロンプト形式
+```
+[INST] <<SYS>>
+You are a helpful, unbiased, uncensored assistant.
+<</SYS>>
+<ユーザの入力した文章>[/INST]<モデルの出力した文章>
+```
+
+
+### プロンプトタイプ名 `codellama`
+プロンプト形式
+```
+[INST] Write code to solve the following coding problem that obeys the constraints and passes the example test cases. Please wrap your code answer using ```:
+<ユーザの入力した文章>
+[/INST]<モデルの出力した文章>
+```
+
+
+### プロンプトタイプ名 `elyzallama2`
+プロンプト形式
+```
+<s>[INST] <<SYS>>
+あなたは誠実で優秀な日本人のアシスタントです。
+<</SYS>>
+
+<ユーザの入力した文章>[/INST]<モデルの出力した文章></s>
+```
+
+
+### プロンプトタイプ名 `beluga`
+プロンプト形式
+```
+### System:
+You are Stable Beluga, an AI that follows instructions extremely well. Help as much as you can. Remember, be safe, and don't do anything illegal.
+
+### User:
+<ユーザの入力した文章>
+
+### Assistant:
+<モデルの出力した文章>
+```
+
+
+### プロンプトタイプ名 `ja-stablelm`
+プロンプト形式
+```
+以下は、タスクを説明する指示と、文脈のある入力の組み合わせです。要求を適切に満たす応答を書きなさい
+
+### 指示: <ユーザの入力した文章>
+### 応答: <モデルの出力した文章>
+```
+
+
+### プロンプトタイプ名 `qa`
+プロンプト形式
+```
+Q: <ユーザの入力した文章>
+A: <モデルの出力した文章>
+```
+
+# ライセンス
+
+Japanese LLM Simple WebUI is under [MIT license](https://en.wikipedia.org/wiki/MIT_License).
---
+++ gguf-webui.py
@@ -0,0 +1,451 @@
+#!/bin/env python3
+
+import argparse
+import gradio as gr
+import time
+import numpy as np
+import os
+import re
+
+
+#------
+# 設定
+#------
+
+# バージョン
+VERSION = "1.0.0"
+
+# ページの最上部に表示させたいタイトルを設定
+TITLE_STRINGS = "GGUF Chat"
+
+# モデルタイプ("llama")
+MODEL_TYPE = "llama"
+# ベースモデルファイルを指定
+BASE_MODEL = "/path/to/model_file.gguf"
+
+# コンテキストサイズの指定
+CONTEXT_SIZE = 4096
+# バッチサイズの指定
+BATCH_SIZE = 512
+
+# GPUを使用するかどうか
+USE_GPU = "off"
+# GPUで処理するレイヤ数
+GPU_LAYERS = 40
+
+# 複数GPUで分割処理する割合指定
+TENSOR_SPLIT = None
+
+# CPUで処理する場合の、使用CPUコア数(0:自動で決定)
+THREAD_NUM = 0
+
+# MMAPが使用できる場合に使用するか
+USE_MMAP = "off"
+# MLOCKを使用するか
+USE_MLOCK = "on"
+
+# プロンプトタイプ("rinna","vicuna","alpaca","llama2","openbuddy","airoboros","beluga","ja-stablelm","qa","none")
+PROMPT_TYPE = "rinna"
+# プロンプトが何トークンを超えたら履歴を削除するか
+PROMPT_THRESHOLD = 4096
+# 履歴を削除する場合、何トークン未満まで削除するか
+PROMPT_DELETED = 2048
+
+# 繰り返しペナルティ(大きいほど同じ繰り返しを生成しにくくなる)
+REPETITION_PENALTY = 1.1
+# 推論時に生成する最大トークン数
+MAX_NEW_TOKENS = 1024
+# 推論時の出力の多様さ(大きいほどバリエーションが多様になる)
+TEMPERATURE = 0.7
+
+# Rope Scalingに関する設定
+ROPE_BASE=10000.0
+ROPE_SCALE=1.0
+
+# WebUIがバインドするIPアドレス
+GRADIO_HOST = '127.0.0.1'
+# WebUIがバインドするポート番号
+GRADIO_PORT = 7860
+
+# WebUI上に詳細設定を表示するか
+SETTING_VISIBLE = "on"
+
+# デバッグメッセージを標準出力に表示するか("on","off")
+DEBUG_FLAG = "on"
+
+
+#------------------
+# クラス、関数定義
+#------------------
+
+def user(message, history):
+    if history == None:
+        history = []
+    # Append the user's message to the conversation history
+    return "", history + [[message, ""]]
+
+# Regenerateボタンクリック時の動作
+def regen(history):
+    if len(history) == 0:
+        return "", [["", ""]]
+    else:
+        history[-1][1]=""
+        return history[-1][0], history
+
+# Remove lastボタンクリック時の動作
+def remove_last(history):
+    if len(history) == 0:
+        return "", [["", ""]]
+    else:
+        history.pop(-1)
+        return history
+
+# プロンプト文字列を生成する関数
+def prompt(curr_system_message, history):
+
+    # Vicuna形式のプロンプト生成
+    if PROMPT_TYPE == "vicuna":
+        prefix = f"""A chat between a curious user and an artificial intelligence assistant.{new_line}The assistant gives helpful, detailed, and polite answers to the user's questions.{new_line}{new_line}"""
+        messages = curr_system_message + \
+            new_line.join([new_line.join(["USER: "+item[0], "ASSISTANT: "+item[1]])
+                    for item in history])
+        messages = prefix + messages
+    # Alpaca形式のプロンプト生成
+    elif PROMPT_TYPE == "alpaca":
+        prefix = f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.{new_line}{new_line}"""
+        messages = curr_system_message + \
+            f"{new_line}{new_line}".join([new_line.join([f"### Instruction:{new_line}"+item[0], f"{new_line}### Response:{new_line}"+item[1]])
+                    for item in history])
+        messages = prefix + messages
+    # Llama2 Chat形式のプロンプト生成
+    elif PROMPT_TYPE == "llama2":
+        prefix = f"""System: You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.{new_line}"""
+        messages = curr_system_message + \
+            new_line.join([new_line.join([f"User: "+item[0], f"Assistant: "+item[1]])
+                    for item in history])
+        messages = prefix + messages
+    # OpenBuddy形式のプロンプト生成
+    elif PROMPT_TYPE == "openbuddy":
+        prefix = f"""You are a helpful, respectful and honest INTP-T AI Assistant named Buddy. You are talking to a human User.{new_line}Always answer as helpfully and logically as possible, while being safe. Your answers should not include any harmful, political, religious, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.{new_line}If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.{new_line}You like to use emojis. You can speak fluently in many languages, for example: English, Chinese.{new_line}You cannot access the internet, but you have vast knowledge, cutoff: 2021-09.{new_line}You are trained by OpenBuddy team, (https://openbuddy.ai, https://github.com/OpenBuddy/OpenBuddy), you are based on LLaMA and Falcon transformers model, not related to GPT or OpenAI.{new_line}{new_line}"""
+        messages = curr_system_message + \
+            new_line.join([new_line.join([f"User: "+item[0], f"Assistant: "+item[1]])
+                    for item in history])
+        messages = prefix + messages
+    # Airoboros形式のプロンプト生成
+    elif PROMPT_TYPE == "airoboros":
+        prefix = f"""[INST] <<SYS>>{new_line}You are a helpful, unbiased, uncensored assistant.{new_line}<</SYS>>{new_line}"""
+        messages = curr_system_message + \
+            f"</s><s>".join(["".join([f"[INST]"+item[0], f"[/INST] "+item[1]])
+                    for item in history]).replace(r'[INST]','',1)
+        messages = prefix + messages
+    # Code Llama形式のプロンプト生成
+    elif PROMPT_TYPE == "codellama":
+        prefix = f"""[INST] Write code to solve the following coding problem that obeys the constraints and passes the example test cases. Please wrap your code answer using ```:{new_line}"""
+        messages = curr_system_message + \
+            f"</s><s>".join(["".join([f"[INST]"+item[0], f"[/INST]"+item[1]])
+                    for item in history]).replace(r'[INST]','',1)
+        messages = prefix + messages
+    # ELYZA japanese Llama2形式のプロンプト生成
+    elif PROMPT_TYPE == "elyzallama2":
+        prefix = f"""<s>[INST] <<SYS>>
+あなたは誠実で優秀な日本人のアシスタントです。
+<</SYS>>{new_line}{new_line}"""
+        messages = curr_system_message + \
+            f"</s><s>".join(["".join([f"[INST]"+item[0], f"[/INST]"+item[1]])
+                    for item in history]).replace(r'[INST]','',1)
+        messages = prefix + messages
+    # StableBeluga2形式のプロンプト生成
+    elif PROMPT_TYPE == "beluga":
+        prefix = f"""### System:{new_line}You are Stable Beluga, an AI that follows instructions extremely well. Help as much as you can. Remember, be safe, and don't do anything illegal.{new_line}{new_line}"""
+        messages = curr_system_message + \
+            f"{new_line}{new_line}".join([new_line.join([f"### User:{new_line}"+item[0], f"{new_line}### Assistant:{new_line}"+item[1]])
+                    for item in history])
+        messages = prefix + messages
+    # Japanese StableLM形式のプロンプト生成
+    elif PROMPT_TYPE == "ja-stablelm":
+        prefix = f"""以下は、タスクを説明する指示と、文脈のある入力の組み合わせです。要求を適切に満たす応答を書きなさい。{new_line}{new_line}"""
+        messages = curr_system_message + \
+            f"{new_line}".join([new_line.join([f"### 指示: "+item[0], f"### 応答: "+item[1]])
+                    for item in history])
+        messages = prefix + messages
+    # Q&A形式のプロンプト生成
+    elif PROMPT_TYPE == "qa":
+        messages = curr_system_message + \
+            new_line.join([new_line.join(["Q: "+item[0], "A: "+item[1]])
+                    for item in history])
+    # 特定の書式を使用しない(入力した文章の続きを生成する)場合のプロンプト生成
+    elif PROMPT_TYPE == "none":
+        messages = curr_system_message + \
+            "".join(["".join([item[0], item[1]])
+                    for item in history])
+    # PROMPT_TYPE設定が正しくなければ終了する
+    else:
+        print(f"Invalid PROMPT_TYPE \"{PROMPT_TYPE}\"")
+        exit()
+    # 生成したプロンプト文字列を返す
+    return messages
+
+
+def chat(curr_system_message, history, p_temperature, p_top_k, p_top_p, p_repetition_penalty, p_max_new_tokens):
+
+    # 会話履歴を表示
+    if DEBUG_FLAG:
+        print(f"history={history}\n")
+
+    # プロンプト文字列生成
+    del_flag = 0
+    while True:
+        # プロンプト文字列を生成する
+        messages = prompt(curr_system_message, history)
+        # もしプロンプトの文字数が多すぎる場合は削除フラグを設定
+        if del_flag == 0 and len(messages) > PROMPT_THRESHOLD:
+            del_flag = 1
+        # 削除フラグが設定され、かつPROMPT_DELETEDより文字数が多い場合は履歴の先頭を削除
+        if del_flag == 1 and len(messages) > PROMPT_DELETED:
+            history.pop(0)
+        # 削除フラグが設定されてないか、設定されているがPROMPT_DELETEDよりトークン数が少ない場合ループを抜ける
+        else:
+            break
+
+    # プロンプトを標準出力に表示
+    if DEBUG_FLAG:
+        print(f"--prompt strings--\n{messages}\n----\n")
+
+    # モデルに入力して回答を生成(ストリーミング出力させる)
+    streamer = m.create_completion(
+                   messages,
+                   max_tokens=p_max_new_tokens,
+                   temperature=p_temperature,
+                   top_k=p_top_k,
+                   top_p=p_top_p,
+                   repeat_penalty=p_repetition_penalty,
+                   stream=True,
+                   stop=["</s>"],
+               )
+
+    #print(history)
+    # Initialize an empty string to store the generated text
+    partial_text = ""
+    for msg in streamer:
+        temp = msg['choices'][0]
+        if 'text' in temp:
+            #print(temp['text'])
+            partial_text += temp['text']
+            history[-1][1] = partial_text
+            # Yield an empty string to cleanup the message textbox and the updated conversation history
+            yield history
+    if DEBUG_FLAG:
+        print(f"--generated strings--\n{partial_text}\n----\n")
+    #return partial_text
+    return history
+
+
+#------
+# 実行
+#------
+
+# 引数を取得
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", type=str, default=BASE_MODEL, help="モデル名またはディレクトリのパス")
+parser.add_argument("--model-type", type=str, choices=["llama"],  default=MODEL_TYPE, help="モデルタイプ名")
+parser.add_argument("--context-size", type=int, default=CONTEXT_SIZE, help="コンテキストサイズ")
+parser.add_argument("--batch-size", type=int, default=BATCH_SIZE, help="バッチサイズ")
+parser.add_argument("--gpu", type=str, choices=["on", "off"], default=USE_GPU, help="GPUを使用するかどうか(使用にはcublas対応のllama-cpp-pythonが必要)")
+parser.add_argument("--gpu-layers", type=int, default=GPU_LAYERS, help="GPUで処理するレイヤ数")
+parser.add_argument("--tensor-split", type=str, default=TENSOR_SPLIT, help="複数GPUでの分割指定(メモリ使用量の比をカンマ区切りの少数で指定する)")
+parser.add_argument("--threads", type=int, default=THREAD_NUM, help="使用するCPUコア数")
+parser.add_argument("--use-mmap", type=str, choices=["on", "off"], default=USE_MMAP, help="mmapが使用可能な場合に使用するか")
+parser.add_argument("--use-mlock", type=str, choices=["on", "off"], default=USE_MLOCK, help="mlockを使用するか")
+parser.add_argument("--prompt-type", type=str, choices=["rinna", "vicuna", "alpaca", "llama2", "openbuddy", "airoboros", "codellama", "elyzallama2", "beluga", "ja-stablelm", "qa", "none"], default=PROMPT_TYPE, help="プロンプトタイプ名")
+parser.add_argument("--prompt-threshold", type=int, default=PROMPT_THRESHOLD, help="このトークン数を超えたら古い履歴を削除")
+parser.add_argument("--prompt-deleted", type=int, default=PROMPT_DELETED, help="古い履歴削除時にこのトークン以下にする")
+parser.add_argument("--repetition-penalty", type=float, default=REPETITION_PENALTY, help="繰り返しに対するペナルティ")
+parser.add_argument("--max-new-tokens", type=int, default=MAX_NEW_TOKENS, help="推論時に生成するトークン数の最大")
+parser.add_argument("--setting-visible", type=str, choices=["on", "off"], default=SETTING_VISIBLE, help="詳細設定を表示するかどうか")
+parser.add_argument("--temperature", type=float, default=TEMPERATURE, help="生成する文章の多様さ")
+parser.add_argument("--rope-freq-base", type=float, default=ROPE_BASE, help="Rope SamplingのBase Frequency")
+parser.add_argument("--rope-freq-scale", type=float, default=ROPE_SCALE, help="Rope ScalingのSacle Factor")
+parser.add_argument("--host", type=str, default=GRADIO_HOST, help="WebサーバがバインドするIPアドレスorホスト名")
+parser.add_argument("--port", type=int, default=GRADIO_PORT, help="Webサーバがバインドするポート番号")
+parser.add_argument("--title", type=str, default=TITLE_STRINGS, help="Webページのタイトル")
+parser.add_argument("--debug", type=str, choices=["on", "off"], default=DEBUG_FLAG, help="デバッグメッセージを標準出力に表示")
+args = parser.parse_args()
+
+# 引数でセットされた値で上書きする
+BASE_MODEL = args.model
+MODEL_TYPE = args.model_type
+CONTEXT_SIZE = args.context_size
+BATCH_SIZE = args.batch_size
+USE_GPU = args.gpu
+GPU_LAYERS = args.gpu_layers
+THREAD_NUM = args.threads
+USE_MMAP = args.use_mmap
+USE_MLOCK = args.use_mlock
+PROMPT_TYPE = args.prompt_type
+PROMPT_THRESHOLD = args.prompt_threshold
+PROMPT_DELETED = args.prompt_deleted
+REPETITION_PENALTY=args.repetition_penalty
+MAX_NEW_TOKENS = args.max_new_tokens
+SETTING_VISIBLE = args.setting_visible
+TEMPERATURE = args.temperature
+ROPE_BASE = args.rope_freq_base
+ROPE_SCALE = args.rope_freq_scale
+GRADIO_HOST = args.host
+GRADIO_PORT = args.port
+TITLE_STRINGS = args.title
+DEBUG_FLAG = args.debug
+
+# tensor_splitの処理
+if args.tensor_split == None or args.tensor_split == "None" or args.tensor_split == "none":
+    TENSOR_SPLIT = None
+else:
+    if re.match(r'^[0-9,\.]+$',args.tensor_split):
+        temp_list = args.tensor_split.split(',')
+        TENSOR_SPLIT = [float(s) for s in temp_list]
+    else:
+        print("Invalid TENSOR_SPLIT value.")
+        exit()
+
+# パラメータ表示
+print("---- パラメータ ----")
+print(f"モデル名orパス: {BASE_MODEL}")
+print(f"モデルタイプ名: {MODEL_TYPE}")
+print(f"コンテキストサイズ: {CONTEXT_SIZE}")
+print(f"バッチサイズ: {BATCH_SIZE}")
+print(f"GPU使用: {USE_GPU}")
+print(f"GPUレイヤ数: {GPU_LAYERS}")
+print(f"GPU分割指定: {TENSOR_SPLIT}")
+print(f"使用CPUコア数: {THREAD_NUM}")
+print(f"mmap使用: {USE_MMAP}")
+print(f"mlock使用: {USE_MLOCK}")
+print(f"プロンプトタイプ: {PROMPT_TYPE}")
+print(f"プロンプトトークン数しきい値: {PROMPT_THRESHOLD}")
+print(f"プロンプトトークン数削除値: {PROMPT_DELETED}")
+print(f"繰り返しペナルティ: {REPETITION_PENALTY}")
+print(f"生成最大トークン数: {MAX_NEW_TOKENS}")
+print(f"詳細設定表示: {SETTING_VISIBLE}")
+print(f"Temperature: {TEMPERATURE}")
+print(f"Rope Sampling Frequency: {ROPE_BASE}")
+print(f"Rope Sampling Scale: {ROPE_SCALE}")
+print(f"WebサーバIPorホスト名: {GRADIO_HOST}")
+print(f"Webサーバポート番号: {GRADIO_PORT}")
+print(f"Webページタイトル: {TITLE_STRINGS}")
+print(f"デバッグ: {DEBUG_FLAG}\n")
+
+# USE_GPUはTrue or Falseに変換
+if USE_GPU == "on":
+    USE_GPU = True
+else:
+    USE_GPU = False
+# USE_MMAPはTrue or Falseに変換
+if USE_MMAP == "on":
+    USE_MMAP = True
+else:
+    USE_MMAP = False
+# USE_MLOCKはTrue or Falseに変換
+if USE_MLOCK == "on":
+    USE_MLOCK = True
+else:
+    USE_MLOCK = False
+# SETTING_VISIBLEはTrue or Falseに変換
+if SETTING_VISIBLE == "on":
+    SETTING_VISIBLE = True
+else:
+    SETTING_VISIBLE = False
+# DEBUG_FLAGはTrue or Falseに変換
+if DEBUG_FLAG == "on":
+    DEBUG_FLAG = True
+else:
+    DEBUG_FLAG = False
+# スレッド数が0の場合Noneに置き換える
+if THREAD_NUM == 0:
+    THREAD_NUM = None
+
+
+## モデルタイプによる設定とモデルのロード
+
+# Llama系モデルの場合
+if MODEL_TYPE == "llama":
+    from llama_cpp import Llama
+    # 改行を示す文字の設定
+    new_line = "\n"
+    # モデルのロード
+    print(f"Starting to load the model \"{BASE_MODEL}\" to memory")
+    # GPUで処理する場合
+    if USE_GPU:
+        m = Llama(
+              model_path=BASE_MODEL,
+              n_ctx=CONTEXT_SIZE,
+              n_threads=THREAD_NUM,
+              n_gpu_layers=GPU_LAYERS,
+              rope_freq_base=ROPE_BASE,
+              rope_freq_scale=ROPE_SCALE,
+              tensor_split=TENSOR_SPLIT,
+              n_batch=BATCH_SIZE,
+            )
+    # CPUで処理する場合
+    else:
+        m = Llama(
+              model_path=BASE_MODEL,
+              n_ctx=CONTEXT_SIZE,
+              n_threads=THREAD_NUM,
+              rope_freq_base=ROPE_BASE,
+              rope_freq_scale=ROPE_SCALE,
+              use_mmap=USE_MMAP,
+              use_mlock=USE_MLOCK,
+            )
+    print(f"Sucessfully loaded the model to the memory")
+# MODEL_TYPE設定が正しくなければ終了する
+else:
+    print(f"Invalid MODEL_TYPE \"{MODEL_TYPE}\"")
+    exit()
+
+# プロンプトの先頭に付加する文字列
+start_message = ""
+
+# Webページ
+with gr.Blocks(title="GGUF Simple WebUI", theme=gr.themes.Base()) as demo:
+    history = gr.State([])
+    gr.Markdown(f"## {TITLE_STRINGS}")
+    chatbot = gr.Chatbot(height=500)
+    with gr.Row():
+        with gr.Column(scale=20):
+            msg = gr.Textbox(label="Chat Message Box", placeholder="Chat Message Box",
+                             show_label=False, container=False)
+        with gr.Column(scale=1, min_width=100):
+            submit = gr.Button("Submit")
+    with gr.Row():
+                stop = gr.Button("Stop")
+                regenerate = gr.Button("Regenerate")
+                removelast = gr.Button("Remove last")
+                clear = gr.Button("Clear")
+    with gr.Accordion(label="Advanced Settings", open=False, visible=SETTING_VISIBLE):
+        with gr.Blocks():
+            p_temperature = gr.Slider(minimum=0.1, maximum=1.0, value=TEMPERATURE, step=0.1, label="Temperature", interactive=True)
+            p_top_k = gr.Slider(minimum=0, maximum=1000, value=0, step=1, label="Top_K (0=無効)", interactive=True)
+            p_top_p = gr.Slider(minimum=0.01, maximum=1.00, value=1.00, step=0.01, label="Top_P (1.00=無効)", interactive=True)
+        with gr.Blocks():
+            p_max_new_tokens = gr.Slider(minimum=1, maximum=4096, value=MAX_NEW_TOKENS, step=1, label="Max New Tokens", interactive=True)
+            p_repetition_penalty = gr.Slider(minimum=1.00, maximum=5.00, value=REPETITION_PENALTY, step=0.01, label="Repetition Penalty (1.00=ペナルティなし)", interactive=True)
+
+    system_msg = gr.Textbox(
+        start_message, label="System Message", interactive=False, visible=False)
+
+    submit_event = msg.submit(fn=user, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False).then(
+        fn=chat, inputs=[system_msg, chatbot, p_temperature, p_top_k, p_top_p, p_repetition_penalty, p_max_new_tokens], outputs=[chatbot], queue=True)
+
+    submit_click_event = submit.click(fn=user, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False).then(
+        fn=chat, inputs=[system_msg, chatbot, p_temperature, p_top_k, p_top_p, p_repetition_penalty, p_max_new_tokens], outputs=[chatbot], queue=True)
+
+    regenerate_click_event = regenerate.click(fn=regen, inputs=[chatbot], outputs=[msg, chatbot], queue=False).then(
+               lambda: None, None, [msg], queue=False).then(
+                   fn=chat, inputs=[system_msg, chatbot, p_temperature, p_top_k, p_top_p, p_repetition_penalty, p_max_new_tokens], outputs=[chatbot], queue=True)
+
+    stop.click(fn=None, inputs=None, outputs=None, cancels=[submit_event, submit_click_event, regenerate_click_event], queue=False)
+
+    removelast.click(fn=remove_last, inputs=[chatbot], outputs=[chatbot], queue=False)
+
+    clear.click(lambda: None, None, [chatbot], queue=False)
+
+demo.queue(max_size=32, concurrency_count=2)
+demo.launch(server_name=GRADIO_HOST, server_port=GRADIO_PORT, share=False)
