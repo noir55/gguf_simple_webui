39f1610cac87e89dfa3e84f1b1936f9a3f4d64db
noir55 <bl_phil@nifty.com>
2024-04-07 23:36:45 +0900

    プロンプトタイプ command-r の追加

--- gguf-webui.py
+++ gguf-webui.py
@@ -47,7 +47,7 @@ USE_MMAP = "off"
 # MLOCKを使用するか
 USE_MLOCK = "on"
 
-# プロンプトタイプ("rinna","vicuna","alpaca","llama2","openbuddy","airoboros","beluga","ja-stablelm","mixtral","swallow","nekomata","elyzallama2","karakuri","gemma", "chatml","qa","none")
+# プロンプトタイプ("rinna","vicuna","alpaca","llama2","openbuddy","airoboros","beluga","ja-stablelm","mixtral","swallow","nekomata","elyzallama2","karakuri","gemma", "chatml","command-r","qa","none")
 PROMPT_TYPE = "rinna"
 # プロンプトが何トークンを超えたら履歴を削除するか
 PROMPT_THRESHOLD = 4096
@@ -211,6 +211,13 @@ def prompt(curr_system_message, history):
             f"<|im_end|>{new_line}<|im_start|>".join(["".join([f"User{new_line}"+item[0], f"<|im_end|>{new_line}<|im_start|>Assistant{new_line}"+item[1]])
                     for item in history])
         messages = prefix + messages
+    # Command R形式のプロンプト生成
+    elif PROMPT_TYPE == "command-r":
+        prefix = f"""<BOS_TOKEN><|START_OF_TURN_TOKEN|>"""
+        messages = curr_system_message + \
+            f"<|END_OF_TURN_TOKEN|><|START_OF_TURN_TOKEN|>".join(["<|END_OF_TURN_TOKEN|><|START_OF_TURN_TOKEN|>".join([f"<|USER_TOKEN|>"+item[0], f"<|CHATBOT_TOKEN|>"+item[1]])
+                    for item in history])
+        messages = prefix + messages
     # Q&A形式のプロンプト生成
     elif PROMPT_TYPE == "qa":
         messages = curr_system_message + \
@@ -300,7 +307,7 @@ parser.add_argument("--tensor-split", type=str, default=TENSOR_SPLIT, help="複�
 parser.add_argument("--threads", type=int, default=THREAD_NUM, help="使用するCPUコア数")
 parser.add_argument("--use-mmap", type=str, choices=["on", "off"], default=USE_MMAP, help="mmapが使用可能な場合に使用するか")
 parser.add_argument("--use-mlock", type=str, choices=["on", "off"], default=USE_MLOCK, help="mlockを使用するか")
-parser.add_argument("--prompt-type", type=str, choices=["rinna", "vicuna", "alpaca", "llama2", "openbuddy", "airoboros", "codellama", "elyzallama2", "beluga", "ja-stablelm", "mixtral", "swallow", "nekomata", "elyzallama2", "karakuri", "gemma", "chatml", "qa", "none"], default=PROMPT_TYPE, help="プロンプトタイプ名")
+parser.add_argument("--prompt-type", type=str, choices=["rinna", "vicuna", "alpaca", "llama2", "openbuddy", "airoboros", "codellama", "elyzallama2", "beluga", "ja-stablelm", "mixtral", "swallow", "nekomata", "elyzallama2", "karakuri", "gemma", "chatml", "command-r", "qa", "none"], default=PROMPT_TYPE, help="プロンプトタイプ名")
 parser.add_argument("--prompt-threshold", type=int, default=PROMPT_THRESHOLD, help="このトークン数を超えたら古い履歴を削除")
 parser.add_argument("--prompt-deleted", type=int, default=PROMPT_DELETED, help="古い履歴削除時にこのトークン以下にする")
 parser.add_argument("--repetition-penalty", type=float, default=REPETITION_PENALTY, help="繰り返しに対するペナルティ")
